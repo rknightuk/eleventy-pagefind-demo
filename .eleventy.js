dcab0b2c87b91367981e533dae11c02585b85d48
Robb Knight <desk@rknight.me>
2023-01-04 18:14:25 +0000

    Underscore folder won't work on github

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
     })
 
     eleventyConfig.on('eleventy.after', () => {
-        execSync(`npx pagefind --source docs --glob \"**/*.html\"`, { encoding: 'utf-8' })
+        execSync(`npx pagefind --source docs --bundle-dir pagefind --glob \"**/*.html\"`, { encoding: 'utf-8' })
     })
 
     return {
